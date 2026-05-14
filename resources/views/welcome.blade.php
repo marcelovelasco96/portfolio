de9c5987baccb4b9b5b8c26361c92678cecb5df9
Marcelo Velasco <marcelovelasco69@gmail.com>
2026-05-14 09:40:24 -0500

    Use Tailwind CDN for portfolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marcelo Velasco | Backend Developer</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-slate-950 text-slate-100">
