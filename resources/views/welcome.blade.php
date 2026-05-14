ffdc80efeb63b192c46b35c374eaf0514a3b8525
Marcelo Velasco <marcelovelasco69@gmail.com>
2026-05-14 09:44:41 -0500

    Update portfolio title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marcelo Velasco | Backend Developer</title>
+    <title>Marcelo Velasco | Sistemas y Soluciones Digitales</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
